cdcde3f444b82046b23717705f5a56694a7af894
Ben Jaeger <benjaeger99@gmail.com>
2021-10-13 17:19:27 +0100

    Moved LFnet to separate py file

--- LFnet.py
+++ LFnet.py
@@ -1,1 +1,28 @@
-"""Will eventually house the class version of the cleaned up code"""
+from torch import nn
+
+
+class LFnet(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.layers = []
+            self.layers.append(nn.Identity())  # use Identity layer as place holder until layers get added
+            self.model = nn.Sequential(*self.layers)
+
+        def forward(self, x):
+            return self.model(x)
+
+        def add_layer(self, **kwargs):
+            # freeze all pre existing layers
+            for layer in self.layers:
+                layer.requires_grad_(requires_grad=False)
+
+            # remove type from kwargs to prevent unexpected argument error
+            type_ = kwargs["type"]
+            layer_kwargs = {key: kwargs[key] for key in kwargs if key != "type"}
+
+            if type_ == "Conv2D":
+                self.layers.append(nn.Conv2d(**layer_kwargs))
+            elif type_ == "Linear":
+                self.layers.append(nn.Linear(**layer_kwargs))
+
+            self.model = nn.Sequential(* self.layers)
--- torch_forward.py
+++ torch_forward.py
@@ -2,6 +2,8 @@ from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from torch import nn, rand
 
+from LFnet import LFnet
+
 
 def get_dataloaders():
     train = MNIST(root="./data/train", train=True, download=True)
@@ -13,42 +15,12 @@ def get_dataloaders():
     return train_dataloader, test_dataloader
 
 
-def make_model():
-    class LFnet(nn.Module):
-        def __init__(self):
-            super().__init__()
-            self.layers = []
-            self.layers.append(nn.Identity())  # use Identity layer as place holder until layers get added
-            self.model = nn.Sequential(*self.layers)
-
-        def forward(self, x):
-            return self.model(x)
-
-        def add_layer(self, **kwargs):
-            # freeze all pre existing layers
-            for layer in self.layers:
-                layer.requires_grad_(requires_grad=False)
-
-            # remove type from kwargs to prevent unexpected argument error
-            type_ = kwargs["type"]
-            layer_kwargs = {key: kwargs[key] for key in kwargs if key != "type"}
-
-            if type_ == "Conv2D":
-                self.layers.append(nn.Conv2d(**layer_kwargs))
-            elif type_ == "Linear":
-                self.layers.append(nn.Linear(**layer_kwargs))
-
-            self.model = nn.Sequential(* self.layers)
-
-    return LFnet()
-
-
 if __name__ == "__main__":
     train_dl, test_dl = get_dataloaders()
-    model = make_model()
+    model = LFnet()
     model(rand(1, 3, 36, 36))
     print(model)
-    model.add_layer(type="Conv2D", kernel_size=(3, 3), in_channels=3, out_channels=6)
+    model.add_layer(type="Conv2D", kernel_size=(3, 3), in_channels=3, out_channels=6, stride=(2,2))
     model(rand(1, 3, 36, 36))
     print(model)
     model.add_layer(type="Conv2D", kernel_size=(3, 3), in_channels=6, out_channels=12)
